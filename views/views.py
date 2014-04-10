e608111ea23a3bf07bf417ec6b30514d19cf670a
lzdhlsc <lzdhlsc@gmail.com>
2014-04-10 17:59:25 -0500

    basic routing

--- views/views.py
+++ views/views.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 from models.forms import SignupForm, LoginForm
 from flask.ext.login import login_user, logout_user, current_user, login_required
 main_app = Blueprint('main_app', __name__)
@@ -24,4 +24,39 @@ def render_signup():
     if request.method == 'POST':
         return redirect(url_for('index'))
 
-    return render_template('signup.html', form = fro)
+    return render_template('signup.html', form = from)
+
+@main_app.route('/group')
+def render_groups():
+    '''page listing groups for a user'''
+    return render_template('groups.html')
+
+@main_app.route('/group/<path:group_id>')
+def render_singe_group(group_id = None):
+    '''page for one single group'''
+    return render_template('single_group.html')
+
+@main_app.route('/group/<path:group_id>/members')
+def render_group_members(group_id = None):
+    '''page for all members in a group'''
+    return render_template('group_members.html')
+
+@main_app.route('/user/<path:user_id>')
+def render_user(user_id = None):
+    '''user page'''
+    return render_template('group_members.html')
+
+@main_app.route('/b/<path:board_id>')
+def render_board(board_id = None):
+    '''board page'''
+    return render_template('board.html')
+
+@main_app.route('/l/<path:list_id>')
+def api_list(list_id = None):
+    return jsonify(None)
+
+@main_app.route('/c/<path:card_id>')
+def api_card(card_id = None):
+    return jsonify(None)
+
+
