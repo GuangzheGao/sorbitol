37fbea8bbf3e4206089354822bc513093714f154
Guangzhe Gao <ggao2@illinois.edu>
2014-04-11 05:22:47 -0500

    most front interfaces except for inside card

--- views/views.py
+++ views/views.py
@@ -5,4 +5,24 @@ main_app = Blueprint('main_app', __name__)
 
 @main_app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html') #show groups and boards inside
+
+@main_app.route('/group/<groupname>')
+def group_main(groupname):
+    return render_template('group.html') #show boards and members of group
+
+@main_app.route('/b/<boardID>')
+def board(boardID):
+    return render_template('board.html') #show lists(AJAX), card(AJAX), members of board
+
+@main_app.route('/l/<listID>')
+def list(listID):
+    pass; #show lists(AJAX), card(AJAX), members of board
+
+@main_app.route('/c/<cardID>')
+def card(cardID):
+    pass; #show things inside card
+
+@main_app.route('/login', methods=['GET', 'POST'])
+def route():
+    return render_template('login_sign_up.html');
