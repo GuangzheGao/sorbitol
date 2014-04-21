eb5ef3336be09d66a626f51da075f0378213eab2
Guangzhe Gao <ggao2@illinois.edu>
2014-04-21 00:56:03 -0500

    database setup, firebase framework, in board interaction

---
+++ models/card.py
@@ -0,0 +1,1 @@
+__author__ = 'Guangzhe Gao'
---
+++ models/comment.py
@@ -0,0 +1,12 @@
+__author__ = 'Guangzhe Gao'
+
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
