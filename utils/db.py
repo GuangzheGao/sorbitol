0136c7228e155c5433bb5aa35dee1076b85faf19
lzdhlsc <lzdhlsc@gmail.com>
2014-04-25 00:17:32 -0500

    fixed bugs due to merge

--- utils/db.py
+++ utils/db.py
@@ -7,7 +7,7 @@ r_server = redis.Redis("localhost")
 
 #mongo_client = MongoClient()
 mysql_engine = create_engine(
-                 'mysql://root:root@localhost/sorbitol?charset=utf8',
+                 'mysql://root:password@localhost/sorbitol?charset=utf8',
                  echo=False)
 mysql_session = sessionmaker(bind=mysql_engine)()
 mysql_session.expire_on_commit = False
